e34db1b7097f1ab52b4dcaab46ff56fd800c93f8
NoahGParker <90000082+NoahGParker@users.noreply.github.com>
2021-09-14 23:36:56 -0400

    Add files via upload

---
+++ Chapter11/CPSC_1110_Ch11_E08_Parker.java
@@ -0,0 +1,14 @@
+/*  CPSC_1110_Ch11_E08_Parker.java: contains class that demonstrates assignment completed
+    Author Noah Parker
+    07/13/2021
+*/
+import java.io.*;
+public class CPSC_1110_Ch11_E08_Parker{
+    // CPSC_1110_Ch11_E08_Parker: Class for demostrating class completion
+    public static void main(String[] args) throws FileNotFoundException{
+        // What It Does:  Proves that the assignment is complete and correct
+        // How It Works: Call to FileCounter.main() demo method
+        FileDemo.main(args);
+    }  
+   
+}
---
+++ Chapter11/CPSC_1110_Ch11_E09_Parker.java
@@ -0,0 +1,15 @@
+/*  CPSC_1110_Ch11_E09_Parker.java: contains class that demonstrates assignment completed
+    Author Noah Parker
+    07/13/2021
+    NOTE: Program to be used with command line arugments "java CPSC_1110_Ch11_E09_Parker ring report.txt address.txt keyword.txt"
+*/
+import java.io.*;
+public class CPSC_1110_Ch11_E09_Parker{
+    // CPSC_1110_Ch11_E09_Parker: Class for demostrating class completion
+    public static void main(String[] args) throws java.io.IOException{
+        // What It Does:  Proves that the assignment is complete and correct
+        // How It Works: Call to FileCounter.main() demo method
+        Find.main(args);
+    }  
+   
+}
